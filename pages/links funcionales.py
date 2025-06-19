eb278108aa30f65834b069a9f2500c528d79ac11
nt <normantinco@gmail.com>
2025-06-19 15:16:31 -0500

    Update

---
+++ pages/links funcionales.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Links SAP")
+
+st.markdown("""
+## Recursos útiles
+
+- [SAP Help Portal - CDS Views](https://help.sap.com/docs/SAP_S4HANA_ON-PREMISE/ee6ff9b281d8448f96b4fe6c89f2bdc8/5418de55938d1d22e10000000a44147b.html)
+
+""")
+
+# Área para que los usuarios compartan links adicionales
+# enlace = st.text_input("Agrega un link útil:")
+# if enlace:
+#     st.write(f"🔗 [{enlace}]({enlace})")
