0913fe0c06bc997ed96130ac8d681cff54a58ff4
nt <normantinco@gmail.com>
2025-06-19 22:16:39 -0500

    Update

--- home.py
+++ home.py
@@ -11,3 +11,10 @@ st.set_page_config(
 st.title("Consultor SAP")
 st.markdown("### ¡Bienvenido!")
 st.info("Utiliza la barra lateral para navegar entre las funcionalidades del sistema.")
+
+st.markdown("""
+## Recursos útiles
+
+- [SAP - CDS Views](https://help.sap.com/docs/SAP_S4HANA_ON-PREMISE/ee6ff9b281d8448f96b4fe6c89f2bdc8/5418de55938d1d22e10000000a44147b.html)
+
+""")
